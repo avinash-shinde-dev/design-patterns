3f05235961b136b5d2316a2b29ff6d489216b565
avinash-shinde-1508 <writeto.avinash.shinde@gmail.com>
2025-03-18 21:37:26 +0530

    strategy behavioural design pattern added

---
+++ src/main/java/behavioral/strategy/traditional/SportVehicle.java
@@ -0,0 +1,9 @@
+package behavioral.strategy.traditional;
+
+public class SportVehicle extends Vehicle{
+
+    @Override
+    public void drive() {
+        System.out.println("Sport drive implementation...");
+    }
+}
---
+++ src/main/java/behavioral/strategy/traditional/Vehicle.java
@@ -0,0 +1,8 @@
+package behavioral.strategy.traditional;
+
+public class Vehicle {
+
+    public void drive() {
+        System.out.println("Normal drive implementation");
+    }
+}
