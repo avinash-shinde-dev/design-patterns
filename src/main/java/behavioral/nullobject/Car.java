383d0da8dc03abe8c98081ef26857cef5521c8db
avinash-shinde-1508 <avinash.shinde.dev@gmail.com>
2025-03-19 22:06:01 +0530

    NullObject behavioural design pattern added

---
+++ src/main/java/behavioral/nullobject/Car.java
@@ -0,0 +1,8 @@
+package behavioral.nullobject;
+
+public class Car implements Vehicle {
+    @Override
+    public int tankCapacity() {
+        return 40;
+    }
+}
---
+++ src/main/java/behavioral/nullobject/Vehicle.java
@@ -0,0 +1,2 @@
+package behavioral.nullobject;public interface Vehicle {
+}
